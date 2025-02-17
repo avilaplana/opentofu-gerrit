232adc6a49ebe4a2921a1fc72a57186c393318d2
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-02-17 18:09:37 +0000

    Add 2 internal ALB

--- aws/ecs/my-test/example.tf
+++ aws/ecs/my-test/example.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "private_alb_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -82,6 +82,24 @@ resource "aws_security_group" "ec2_sg" {
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.private_alb_sg.id]
   }
 
   egress {
@@ -100,8 +118,31 @@ resource "aws_lb" "public_alb" {
   subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
 
-resource "aws_lb_target_group" "internal_tg" {
-  name     = "internal-tg"
+resource "aws_lb" "private_alb_1" {
+  name               = "private-alb-1"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.private_alb_sg.id]
+  subnets            = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
+
+resource "aws_lb" "private_alb_2" {
+  name               = "private-alb-2"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.private_alb_sg.id]
+  subnets            = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
+
+resource "aws_lb_target_group" "internal_tg_1" {
+  name     = "internal-tg-1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group" "internal_tg_2" {
+  name     = "internal-tg-2"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
@@ -113,8 +154,103 @@ resource "aws_lb_listener" "public_alb_listener" {
   protocol        = "HTTP"
 
   default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default action"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "public_alb_rule_1" {
+  listener_arn = aws_lb_listener.public_alb_listener.arn
+  priority     = 1
+
+  action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_lb_target_group.private_alb_1_tg.arn
+      }
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/path1/*"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "public_alb_rule_2" {
+  listener_arn = aws_lb_listener.public_alb_listener.arn
+  priority     = 2
+
+  action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_lb_target_group.private_alb_2_tg.arn
+      }
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/path2/*"]
+    }
+  }
+}
+
+resource "aws_lb_target_group" "private_alb_1_tg" {
+  name     = "private-alb-1-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip" # Since ALBs don’t register as "instance", use "ip"
+}
+
+resource "aws_lb_target_group_attachment" "internal_alb_1_attach" {
+  target_group_arn = aws_lb_target_group.private_alb_1_tg.arn
+  target_id        = aws_lb.private_alb_1.dns_name # Use the ALB's DNS/IP
+  port            = 80
+}
+
+resource "aws_lb_target_group" "private_alb_2_tg" {
+  name     = "private-alb-2-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip" # Since ALBs don’t register as "instance", use "ip"  
+}
+
+resource "aws_lb_target_group_attachment" "internal_alb_2_attach" {
+  target_group_arn = aws_lb_target_group.private_alb_2_tg.arn
+  target_id        = aws_lb.private_alb_2.dns_name # Use the ALB's DNS/IP
+  port            = 80
+}
+
+resource "aws_lb_listener" "private_alb_1_listener" {
+  load_balancer_arn = aws_lb.private_alb_1.arn
+  port             = 80
+  protocol        = "HTTP"
+
+  default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.internal_tg.arn
+    target_group_arn = aws_lb_target_group.internal_tg_1.arn
+  }
+}
+
+resource "aws_lb_listener" "private_alb_2_listener" {
+  load_balancer_arn = aws_lb.private_alb_2.arn
+  port             = 80
+  protocol        = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.internal_tg_2.arn
   }
 }
 
@@ -145,13 +281,13 @@ resource "aws_instance" "nginx_2" {
 }
 
 resource "aws_lb_target_group_attachment" "nginx_1_attach" {
-  target_group_arn = aws_lb_target_group.internal_tg.arn
+  target_group_arn = aws_lb_target_group.internal_tg_1.arn
   target_id        = aws_instance.nginx_1.id
   port            = 80
 }
 
 resource "aws_lb_target_group_attachment" "nginx_2_attach" {
-  target_group_arn = aws_lb_target_group.internal_tg.arn
+  target_group_arn = aws_lb_target_group.internal_tg_2.arn
   target_id        = aws_instance.nginx_2.id
   port            = 80
 }
